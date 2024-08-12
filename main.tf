7a4f0da1a6c738d63095e15c1a41307b7ea37af8
anjali3soni <anjali@gkmit.co>
2024-08-12 16:39:32 +0530

    final

--- main.tf
+++ main.tf
@@ -1,133 +1,91 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.61.0"
-    }
-  }
-}
-
 provider "aws" {
-  region = "eu-north-1"
+  region = "ap-south-1"
 }
-
-resource "aws_vpc" "my-vpc" {
-  cidr_block = "10.0.0.0/16"
-
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  name = "jenkins-vpc"
+  cidr = "10.0.0.0/16"
+  azs             = ["ap-south-1a"]
+  public_subnets  = ["10.0.101.0/24"]
+  enable_vpn_gateway = true
   tags = {
-    Name = "my-vpc"
+    Terraform = "true"
   }
 }
 
-resource "aws_subnet" "public-sub" {
-  vpc_id     = aws_vpc.my-vpc.id
-  cidr_block = "10.0.1.0/24"
-
-  tags = {
-    Name = "public-sub"
-  }
-}
-
-resource "aws_internet_gateway" "my-igw" {
-  vpc_id = aws_vpc.my-vpc.id
-  tags = {
-    Name = "my-igw"
-  }
-}
-
-resource "aws_route_table" "my-route" {
-  vpc_id = aws_vpc.my-vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my-igw.id
-  }
-}
-
-resource "aws_route_table_association" "pub-sub" {
-  route_table_id = aws_route_table.my-route.id
-  subnet_id      = aws_subnet.public-sub.id
-}
-
-resource "aws_instance" "my-inst" {
-  ami = "ami-07c8c1b18ca66bb07"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.public-sub.id
-  tags = {
-    Name = "my-inst"
-  }
+resource "aws_instance" "public_instance" {
+  ami                       = "ami-0ad21ae1d0696ad58"
+  instance_type             = "t2.micro"
+  subnet_id                 = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-}
-
-resource "random_id" "id-random" {
-  byte_length = 8
-}
-
-resource "aws_s3_bucket" "static-proj" {
-  bucket = "static-proj123"
-
+  key_name                  = "terraform"
+  vpc_security_group_ids    = [aws_security_group.public_sg.id]
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name = "Public_Instance"
+  }
+}
+resource "aws_security_group" "public_sg" {
+  name        = "public_sg"
+  description = "Allow inbound SSH and HTTP traffic"
+  vpc_id = module.vpc.vpc_id
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+  tags = {
+    Name = "public_sg"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "pub-access" {
-  bucket = aws_s3_bucket.static-proj.id
 
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "jenkins34234-s3-bucket-for-index-html-testing-39312345"
+}
+resource "aws_s3_bucket_public_access_block" "publicallow" {
+  bucket = aws_s3_bucket.website_bucket.id
   block_public_acls       = false
   block_public_policy     = false
   ignore_public_acls      = false
   restrict_public_buckets = false
 }
-
-resource "aws_s3_bucket_policy" "static-policy" {
-  bucket = aws_s3_bucket.static-proj.id
-  policy = jsonencode(
-    {
-      Version = "2012-10-17",
-      Statement = [
-        {
-          Sid       = "PublicReadGetObject",
-          Effect    = "Allow",
-          Principal = "*",
-          Action = [
-            "s3:GetObject"
-          ],
-          Resource = [
-            "arn:aws:s3:::${aws_s3_bucket.static-proj.id}/*"
-          ]
-        }
-      ]
-    }
-  )
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  depends_on = [aws_s3_bucket_public_access_block.publicallow]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "PublicReadGetObject"
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+      },
+    ]
+  })
 }
-
-resource "aws_s3_bucket_website_configuration" "static-proj" {
-  bucket = aws_s3_bucket.static-proj.id
-
+resource "aws_s3_object" "index" {
+  bucket        = aws_s3_bucket.website_bucket.bucket
+  key           = "index.html"
+  source        = "./index.html"
+  content_type  = "text/html"
+  etag          = "${md5(file("./index.html"))}"
+}
+resource "aws_s3_bucket_website_configuration" "web-host-config" {
+  bucket = aws_s3_bucket.website_bucket.id
   index_document {
     suffix = "index.html"
   }
 }
-
-resource "aws_s3_object" "index_html" {
-  bucket = aws_s3_bucket.static-proj.bucket
-  source = "./index.html"
-  key    = "index.html"
-  content_type = "text/html"
+output "s3_url" {
+  description = "s3 obj url"
+  value       = aws_s3_bucket_website_configuration.web-host-config.website_endpoint
 }
-
-# resource "aws_s3_object" "style_css" {
-#   bucket = aws_s3_bucket.static-proj.bucket
-#   source = "./style.css"
-#   key    = "style.css"
-#   content_type = "text/css"
-# }
-
-output "link" {
-  value = aws_s3_bucket_website_configuration.static-proj.website_endpoint
-}
-
-
-
